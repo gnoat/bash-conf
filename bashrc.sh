a7bb29de5e31c77eb3356a87f941ca4dbc0f2853
shane stephenson (shalst) <stephenson.shane.a@gmail.com>
2022-11-20 20:02:53 -0500

    initial commit so I

---
+++ bashrc.sh
@@ -0,0 +1,26 @@
+######################
+#                    #
+# personal shortcuts #
+#                    #
+######################
+
+# Alias definitions.
+if [ -f ~/.bash_aliases ]; then
+    . ~/.bash_aliases
+fi
+
+# clipboard shortcuts
+alias setclip="xclip -selection c"
+alias getclip="xclip -selection c -o"
+alias pwdc="pwd | setclip"
+
+# sets mc to make a directory and cd into it
+mc() { mkdir $1; cd $1; }
+
+# creates an permanent alias for the current working directory
+aliad() {
+    export aline="alias $1=\"cd "$(pwd)"\""
+    echo "$1 -> $(pwd)"
+    echo "$aline" >> ~/.bash_aliases
+    source ~/.bash_aliases
+}
