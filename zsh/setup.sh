b41b48d4d93822522b4b825be40b50df1d4da3b3
shane stephenson (shalst) <stephenson.shane.a@gmail.com>
2023-01-10 14:11:36 -0500

    added zsh setup and configuration

---
+++ zsh/setup.sh
@@ -0,0 +1,9 @@
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+source ~/.zshrc
+
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
+
+cp -f zshrc ~/.zshrc
